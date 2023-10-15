e8a62439df4f4e6a474607f2ff17513c8a7deeb9
prashanth <prashanth@svl.services>
2023-10-15 18:40:00 +0530

    Recording openAI call latency

--- app.py
+++ app.py
@@ -347,11 +347,10 @@ def conversation_without_data(request_body):
 def conversation():
     request_body = request.json
     time_before = datetime.now()
-    logging.error(f'conversation start time: {time_before}')
+    logging.info(f'conversation start time: {time_before}')
     response = conversation_internal(request_body)
     time_after = datetime.now()
-    logging.exception(" Testing exception prashanth")
-    logging.error(f'Conversation latency: {time_after - time_before}')
+    logging.info(f'Conversation latency: {time_after - time_before}')
     return response
 
 def conversation_internal(request_body):
